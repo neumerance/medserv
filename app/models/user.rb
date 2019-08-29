0b2358df5ddd45b09124b4b3c4d1383459f6f554
jonathan canaveral <neumerance@live.com>
2019-08-29 23:22:12 +0800

    user role and user dashboard name holder

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  enum role: [ :volunter, :admin ]
+
+  def is_admin?
+    role == 'admin'
+  end
 end
